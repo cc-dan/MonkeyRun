2b20dfb61b308d57364806ab8e83dd9180b291f2
Dante <dantecocciolo@gmail.com>
2022-10-25 19:52:44 -0300

    lanzar piedras

--- src/juego/Mono.java
+++ src/juego/Mono.java
@@ -9,12 +9,16 @@ public class Mono {
 	private int vel_vertical;
 	private Entorno entorno;
 	public boolean colision;
+	private Piedra piedra;
+	private Piedra[] piedras;
 	
 	public Mono(int x, int y, Entorno entorno) {
 		this.x = x;
 		this.y = y;
 		this.entorno = entorno;
 		this.colision = false;
+		
+		this.piedra = new Piedra(this.x, this.y, entorno);
 	}
 	
 	public int getX() {
@@ -51,8 +55,23 @@ public class Mono {
 				this.saltar();
 			}
 		}
-
 		this.y += this.vel_vertical;
+		
+		// Lanzar piedra
+		if (this.entorno.sePresiono(entorno.TECLA_ESPACIO) && this.piedra.getVel() == 0) {
+			this.piedra.setX(this.x);
+			this.piedra.setY(this.y);
+			this.piedra.cambiarEstado();
+		}
+		// Reposicionar piedra cuando se sale del mapa o choca un enemigo
+		if ((this.piedra.getX() > this.entorno.ancho() || this.piedra.getY() > this.entorno.alto()) && this.piedra.getVel() > 0) {
+			this.piedra.cambiarEstado();
+		}
+		
+		if (this.piedra.getVel() > 0) {
+			this.piedra.dibujar();
+		}
+		this.piedra.actualizar();
 	}
 	
 	public void dibujar() {
---
+++ src/juego/Piedra.java
@@ -0,0 +1,46 @@
+package juego;
+
+import entorno.Entorno;
+
+public class Piedra {
+	int x, y;
+	int vel = 10;
+	int vel_actual;
+	Entorno entorno;
+	
+	public Piedra(int x, int y, Entorno entorno) {
+		this.x = x;
+		this.y = y;
+		this.entorno = entorno;
+		this.vel_actual = 0;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	public int getY() {
+		return this.y;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getVel() {
+		return this.vel_actual;
+	}
+	
+	public void cambiarEstado() {
+		this.vel_actual = (this.vel_actual - this.vel) * -1;
+	}
+	
+	public void actualizar() {
+		this.x += vel_actual;
+		this.y += vel_actual;
+	}
+	
+	public void dibujar() {
+		this.entorno.dibujarCirculo(this.x, this.y, 8, null);
+	}
+}
